5319b79c566fce1e5715019bc8d755beaf55f9d8
Elijah Wilson <elijah.b.wilson@gmail.com>
2018-01-15 00:03:19 -0600

    updated blog page to three columns with scroll generated via JS. Added focus box at top of page

---
+++ data/blog.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const blogs = [
+    {
+        "title": "Fear",
+        "content": "In hac habitasse platea dictumst. Morbi bibendum eget arcu nec placerat. Sed lorem mauris, viverra sed arcu vel, vestibulum aliquet eros. Quisque sagittis sapien ac malesuada efficitur. Phasellus pellentesque nulla dignissim, rhoncus risus sit amet, porta lacus. Phasellus ac placerat quam. Vivamus et interdum tellus, a scelerisque orci. Vestibulum tincidunt lacus at dapibus tempor. Sed efficitur justo sed tellus ultrices aliquam. Vivamus velit purus, cursus et aliquet ut, varius in sapien. Fusce consequat vestibulum mollis. Pellentesque eget lorem neque. Nullam pretium nisl quis facilisis ultricies.",
+        "date": "November 15th, 2017"
+    },
+    {
+        "title": "Hello Worlds",
+        "content": "Nulla eget nunc vestibulum, cursus nisl vel, interdum tellus. Sed quis orci non ipsum vestibulum porta. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae Donec gravida dui in purus auctor tristique. Praesent feugiat in nisl ac mattis. Phasellus tristique, quam nec volutpat pharetra, eros nibh feugiat massa, sed ornare justo lectus a nisi. Er sapien nisi, porttitor et justo et, pulvinar placerat nunc. Cras in dui euismod, varius lacus ac, dictum justo. Etiam suscipit eu libero vel faucibus. Donec eget fermentum augue. Pellentesque pulvinar velit eu luctus accumsan. Nullam vel nisl eu est luctus malesuada non suscipit sem. Mauris in interdum nibh, vehicula eleifend libero. Morbi vitae ipsum et justo mollis vehicula. Morbi at ornare augue. In eu blandit leo. Morbi vitae urna nulla.",
+        "date": "December 1st, 2017"
+    },
+    {
+        "title": "Mountain after mountain",
+        "content": "Donec varius dignissim lacus, vitae venenatis est rhoncus vel. Nullam et dui non neque dignissim placerat quis sit amet sapien. Praesent suscipit, risus id semper lacinia, lacus ligula volutpat enim, id maximus est ante at enim. In luctus ipsum quis elit imperdiet dictum. Nunc vehicula ultrices est, et dignissim justo tincidunt in. Proin pellentesque mauris non erat consequat porttitor. Curabitur eget suscipit diam. Duis placerat malesuada odio, dictum varius ante vehicula id. Curabitur feugiat hendrerit est, vel ornare enim venenatis at. Nunc eget blandit leo, ac scelerisque turpis. Vivamus lacus eros, maximus et porta vel, convallis eget urna.",
+        "date": "December 15th, 2017"
+    },
+    {
+        "title": "Resolutions",
+        "content": "Etiam dapibus arcu eu lacus vehicula, in sagittis erat pharetra. Morbi vel vestibulum arcu. Fusce pellentesque placerat odio, et mattis leo dignissim interdum. Sed at nibh et nisl blandit tincidunt ac vel ligula. Phasellus dolor dui, mattis sed metus vel, elementum malesuada ligula. Donec semper sem eget dignissim blandit. Aenean commodo leo augue. Proin feugiat commodo enim, a lobortis orci congue suscipit. Cras fringilla neque metus, id tincidunt diam dignissim vel. Nulla mollis enim sit amet iaculis dapibus. In non tempor purus. Vivamus vel elementum lorem, sit amet luctus libero. Nullam sed egestas nisl, eu mattis arcu. Proin ultricies ex vel vulputate condimentum. Aenean massa enim, tempus ac eros ut, mollis lacinia dolor. Sed eget varius magna. Sed tincidunt ultricies laoreet. Vestibulum mattis ante urna, ut auctor mi sagittis vehicula. Praesent ornare risus in neque molestie, sit amet pretium tortor vulputate. Aliquam eleifend urna vel faucibus vestibulum. Duis vehicula bibendum turpis tempor euismod. Donec lobortis risus eu nisi mattis sollicitudin vitae id neque. Sed nunc ex, egestas eget arcu quis, mollis gravida mi.",
+        "date": "December 29th, 2017"
+    },
+    {
+        "title": "How to not give up on a blog",
+        "content": "Etiam eget mi sed nunc scelerisque iaculis ac vitae sapien. Nullam id tellus nisl. Nulla ac risus at arcu finibus imperdiet vel nec lorem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc et ex nisl. Ut interdum placerat risus ac pharetra. Donec elementum metus non tortor laoreet, a pellentesque nunc pellentesque. Mauris pretium molestie sapien non rhoncus. Praesent facilisis placerat leo dapibus mollis. Quisque pulvinar dui nec libero eleifend dictum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In ac libero tincidunt, ornare ex sed, rhoncus nulla. Proin tristique condimentum tristique.",
+        "date": "January 2nd, 2018"
+    }
+]
+
+// module.exports = { blogs };
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,10 @@
+'use strict';
+
+// const blogsLocal = require('../data/blogs.js');
+
 const blogHolder = document.getElementById('blog-holder');
+
+// console.log(blogsLocal);
 
 const xhr = new XMLHttpRequest();
 xhr.open("GET", "../data/blog-posts.json");
@@ -9,7 +15,7 @@ xhr.addEventListener('load', function() {
         var blogPosts = JSON.parse(this.responseText);
         for (let i = 0; i < blogPosts.length; i++) {
             let blogBox = document.createElement('div');
-            blogBox.classList.add('blog-box');
+            blogBox.classList.add('blog-box');          
             let header = document.createElement('h4');
             let dater = document.createElement('h6');
             let body = document.createElement('p');
@@ -25,3 +31,23 @@ xhr.addEventListener('load', function() {
         }
     }
 });
+
+document.getElementsByClassName("blog-box").addEventListener("click", () => {
+
+});
+
+// Local storage work for next version ------------------
+
+// const xhrLocal = new XMLHttpRequest();
+// xhrLocal.open("GET", "../data/blog-posts.json");
+// xhrLocal.send();
+
+// var posts = { 'one': 1, 'two': 2, 'three': 3 };
+
+// // Put the object into storage
+// localStorage.setItem('posts', JSON.stringify(xhrLocal));
+
+// // Retrieve the object from storage
+// var retrievedPosts = localStorage.getItem('posts');
+
+// console.log('retrievedPosts: ', JSON.parse(retrievedPosts));

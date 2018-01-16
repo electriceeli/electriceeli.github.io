d54acc912db8a1a37b77af32c971942870f78396
Elijah Wilson <elijah.b.wilson@gmail.com>
2018-01-16 11:04:49 -0600

    cleaned up project and seperated blog JS from main JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,6 @@
 'use strict';
 
-const blogHolder = document.getElementById('blog-holder');
 
-const xhr = new XMLHttpRequest();
-xhr.open("GET", "../data/blog-posts.json");
-xhr.send();
-
-xhr.addEventListener('load', function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var blogPosts = JSON.parse(this.responseText);
-        for (let i = 0; i < blogPosts.length; i++) {
-            let blogBox = document.createElement('div');
-            blogBox.classList.add('blog-box');          
-            let header = document.createElement('h4');
-            let dater = document.createElement('h6');
-            let body = document.createElement('p');
-            let rule = document.createElement('hr');
-            blogHolder.appendChild(blogBox);
-            blogBox.classList.add('project-item-blog');
-            blogBox.appendChild(header).textContent = blogPosts[i].title;
-            header.classList.add('title');
-            blogBox.appendChild(dater).textContent = blogPosts[i].date;
-            dater.appendChild(rule);
-            rule.classList.add('white-pls');
-            blogBox.appendChild(body).textContent = blogPosts[i].content;
-            blogBox.setAttribute("id", "post-body");
-
-            let blogPostCollection = document.getElementsByClassName("blog-box");
-            
-            for (let x = 0; x < blogPostCollection.length; x++) {
-                blogPostCollection[x].addEventListener("click", function() {
-                    document.getElementById("focus-box").style.display = "block";
-                    document.getElementById("focus-box").innerHTML = this.innerHTML;
-                });
-            };
-        }
-    }
-});
 
 
 // Local storage work for next version ------------------

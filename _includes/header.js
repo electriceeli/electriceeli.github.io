707c57c9aaef1e7d0c39702c1b6206d070d8fd82
Eli Wilson <elijah.b.wilson@gmail.com>
2021-01-01 14:33:26 -0500

    use blog template via CSStricks

---
+++ _includes/header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header>
+      <p>Built with Next.js</p>
+    </header>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,41 @@
-function HomePage() {
-  return <div>🚧 Under construction. Come back soon! 🚧</div>;
+import DefaultLayout from "@layouts/default";
+import Link from "next/link";
+import { getConfig, getAllPosts } from "@api";
+
+export async function getStaticProps() {
+  const config = await getConfig();
+  const allPosts = await getAllPosts();
+  return {
+    props: {
+      posts: allPosts,
+      title: config.title,
+      description: config.description
+    }
+  };
 }
 
-export default HomePage;
+export default function Blog(props) {
+  return (
+    <DefaultLayout title={props.title} description={props.description}>
+      <hr />
+      <p>🚧 Under construction. Come back soon! 🚧</p>
+      <p>Signed,</p>
+      <p>Eli :)</p>
+      <hr />
+      {/* TODO: Build page then actually use the posts */}
+      {/* Built with https://css-tricks.com/building-a-blog-with-next-js/ */}
+      {/* TODO: Build a separate portfolio page that shows games/showGO */}
+      {/* <ul>
+        {props.posts.map(function(post, idx) {
+          return (
+            <li key={idx}>
+              <Link href={"/posts/" + post.slug}>
+                <a>{post.title}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul> */}
+    </DefaultLayout>
+  );
+}
